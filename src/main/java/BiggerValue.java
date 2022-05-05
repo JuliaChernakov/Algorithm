413e6af70f51cf166eec02acb16fd0b570d18eb1
Julia Chernakov <julia.chernakov@gmail.com>
2022-05-06 01:17:41 +0300

    clear code

--- src/main/java/BiggerValue.java
+++ src/main/java/BiggerValue.java
@@ -2,6 +2,13 @@ public class BiggerValue {
 //    Напишите алгоритм BiggerValue, который из двух параметров типа int возвращает бОльшее значение.
 
     public int biggerValueAlgorithm(int number1, int number2) {
-        return Math.max(number1, number2);
+//        return Math.max(number1, number2);
+
+        int result = number1;
+        if (number2 > result) {
+            result = number2;
+        }
+
+        return result;
     }
 }
--- src/main/java/SumArray.java
+++ src/main/java/SumArray.java
@@ -4,12 +4,14 @@ public class SumArray {
 
     public double sumArrayAlgorithm(double[] array) {
         if (array.length == 0) {
+
             return Double.MIN_VALUE;
         }
         double sum = 0;
         for (int i = 0; i < array.length; i++) {
             sum = sum + array[i];
         }
+
         return sum;
     }
 }
