a471e43407992cee37267e4411b6de56527cf19b
Julia Chernakov <julia.chernakov@gmail.com>
2022-05-10 11:18:54 +0300

    Edit Code

--- src/main/java/SumOfTwo.java
+++ src/main/java/SumOfTwo.java
@@ -2,25 +2,28 @@ public class SumOfTwo {
 //    Написать алгоритм SumOfTwo, который принимает на вход массив целых чисел,
 //    и число n. Алгоритм  возвращает пары элементов, которые в сумме дают число n.
 
-    public int[][] SumOfTwoAlgorithm (int[] array, int n){
+    public int[][] SumOfTwoAlgorithm(int[] array, int n) {
+        int[][] result = {};
 
-        int count = 0;
-        for (int i = 0; i < array.length; i++) {
-            for (int j = i + 1; j < array.length; j++) {
-                if (array[i] + array[j] == n) {
-                    count++;
+        if (array.length > 0) {
+            int count = 0;
+            for (int i = 0; i < array.length; i++) {
+                for (int j = i + 1; j < array.length; j++) {
+                    if (array[i] + array[j] == n) {
+                        count++;
+                    }
                 }
             }
-        }
 
-        int[][] result = new int[count][2];
-        int r = 0;
-        for (int i = 0; i < array.length; i++) {
-            for (int j = i + 1; j < array.length; j++) {
-                if (array[i] + array[j] == n) {
-                    result[r][0] = array[i];
-                    result[r][1] = array[j];
-                    r++;
+            result = new int[count][2];
+            int r = 0;
+            for (int i = 0; i < array.length; i++) {
+                for (int j = i + 1; j < array.length; j++) {
+                    if (array[i] + array[j] == n) {
+                        result[r][0] = array[i];
+                        result[r][1] = array[j];
+                        r++;
+                    }
                 }
             }
         }
